6b3b18a5f1018a2b85958244d9af5cc5fb4e63df
ligo1626 <sligon@csumb.edu>
2016-11-06 07:15:33 +0000

    Implemented more functionality/ Cart Page is Started/ Some Problems Found

--- displayCart.php
+++ displayCart.php
@@ -1,3 +1,62 @@
+<?php
+
+include '../../includes/dbConnection.php';
+$dbConn = getDatabaseConnection('sportsStore');
+
+
+session_start(); //You must always use this line to start or resume a session
+//session_destroy();
+print_r($_GET['cart']);
+
+ if (!isset($_SESSION['cart'])) {
+     $_SESSION['cart'] = array();  //initializing session variable
+  }
+
+$cart = $_GET['cart'];
+
+$updatedCart = array_unique($cart);
+
+foreach($updatedCart as $element)
+{   
+    if ($element[0] == 'C'){
+         $sql = "SELECT * FROM `clothing` WHERE clothesId =\"". $element . "\"";
+         $statement= $dbConn->prepare($sql); 
+         $statement->execute(); //Always pass the named parameters, if any
+         $records = $statement->fetch(); 
+         
+         echo $records['clothesName'] . " - " . $records['clothesType'] . "<br/>";
+         
+    }
+    
+    else if ($element[0] == 'E'){
+        //  echo "EEEEEE";
+         $sql = "SELECT * FROM `equipment` WHERE equipId =\"". $element . "\"";
+         $statement= $dbConn->prepare($sql); 
+         $statement->execute(); //Always pass the named parameters, if any
+         $records = $statement->fetch(); 
+         
+         if (strcmp ($records['ball'], "NULL")==0){
+             echo $records['equipName'] . " - " . $records['misc'] . "<br/>";
+             continue;
+         }
+         
+         else if (strcmp ($records['misc'],"NULL")==0){
+             $records['equipName'] . " - " . $records['ball'] . "<br/>";
+             continue;
+         }
+         
+        
+    }
+    
+    // echo $element . "<br/>";
+    // if (!in_array($element, $SESSION['cart'])) { //avoid duplicate device Ids
+    //   $_SESSION['cart'][] = $element;
+    // }
+//    echo $element . "<br/>";
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
--- index.php
+++ index.php
@@ -21,9 +21,16 @@ function getClothing(){
     
 }
 
-function getEquipment(){
+// function getEquipment(){
+//      global $dbConn;
+//     $sql = "SELECT DISTINCT(clothesType) FROM `clothing`";
+//     $statement=$dbConn->prepare($sql);
+//     $statement->execute();
+//     $records = $statement->fetchALL();  
     
-}
+//     return $records;
+    
+// }
 
 function getSports(){
     global $dbConn;
@@ -35,16 +42,16 @@ function getSports(){
     return $records;
 }
 
-function search(){
+function searchClothes(){
     
     
     global $dbConn;
     $sql = "SELECT * FROM `clothing` c INNER JOIN `Sports` s 
             ON c.sportId = s.sportId";  //Getting all records 
     
-    if (!empty($_GET['clothingType'])){
-        // echo $_GET['clothingType'];
-        $sql = $sql . " WHERE clothesType = \"" . $_GET['clothingType'] . "\"";
+    if (!empty($_GET['itemType'])){
+        // echo $_GET['itemType'];
+        $sql = $sql . " WHERE clothesType = \"" . $_GET['itemType'] . "\"";
         
         if (!empty($_GET['sportsType'])){
             // echo $_GET['sportsType'];
@@ -72,12 +79,74 @@ function search(){
 
 }
 
-if (isset($_GET['submit'])){
-    //  if (!empty($_GET['sportsType'])){
-    //         echo $_GET['sportsType'];
-    //  }
-    search();  
+function searchEquipBalls(){
+    global $dbConn;
+    $sql = "SELECT * FROM `equipment` e INNER JOIN `Sports` s 
+            ON e.sportId = s.sportId";  //Getting all records 
+            
+    if (!empty($_GET['itemType'])){
+        
+        // echo $_GET['itemType'];
+        
+        if ($_GET['itemType'] == "balls"){
+             if ($_GET['sportsType']=="Baseketball"){
+                $_GET['sportsType']="Basketball"; //Fixing spelling error
+            }
+            
+            if ($_GET['sportsType']=="Soccer"){
+                $_GET['sportsType']="Soccer Ball"; //Fixing spelling error
+            }
+        
+            if (!empty($_GET['sportsType'])){
+                // echo $_GET['sportsType'];
+                $sql = $sql . " WHERE ball= \"" . $_GET['sportsType'] . "\"";
+            }
+        }
+        
+        else if ($_GET['itemType'] ==  "equipment"){
+            // echo "TEST";
+            $sql = $sql . " WHERE misc != 'NULL' AND sportName = \"" . $_GET['sportsType'] . "\"";
+        }
+        
+    }
+    
+    $statement= $dbConn->prepare($sql); 
+    $statement->execute(); //Always pass the named parameters, if any
+    $records = $statement->fetchALL(PDO::FETCH_ASSOC);  
+    
+    foreach($records as $record) {
+              echo"<ul style>";
+              echo "<li> <input type='checkbox' name='cart[]'    value =" . $record['equipId'] . ">";
+              echo  "<a href=\"". $record['link'] . "\"" . ">" . $record['equipName'] . "</a>" . " - ". $record['sportName']. "</li>";
+            //   echo "<br/>";
+              echo"</ul>";
+    }
+    
+    
+    
 }
+
+function goPlace(){
+    if (isset($_GET['submit'])){
+     if (!empty($_GET['itemType'])){
+         
+        //  echo $_GET['itemType'];
+        if (strcmp($_GET['itemType'], "balls")==0 || strcmp($_GET['itemType'], "equipment")==0){
+            searchEquipBalls();
+        }
+        else {
+            searchClothes();
+            
+        }
+     }
+   
+    }
+    else{
+        echo "null";
+    }
+}
+
+
 
 
 ?>
@@ -91,14 +160,18 @@ if (isset($_GET['submit'])){
     <body>
         
         <form>
-            Clothes:
-            <select name ="clothingType">
+            Items:
+            <select name ="itemType">
                 <option value ="default">Select One</option>
+                <option value ="balls">balls</option>
+                <option value ="equipment">equipment</option>
                 <?= $records = getClothing();
                     foreach($records as $record) {
                         echo "<option value='" . $record['clothesType'] . "'>" . $record['clothesType'] . "</option>";
                     }
                 ?>
+                
+                
             </select>
             Sports:
             <select name = "sportsType">
@@ -113,6 +186,13 @@ if (isset($_GET['submit'])){
             </select>
             <input type="submit" name ="submit" value="Search"/>
         </form>
+        
+        <form action="displayCart.php">
+             <?=goPlace()?>
+           <br />
+           <input type="submit" value="Continue">
+         </form>
+       
 
     </body>
 </html>
